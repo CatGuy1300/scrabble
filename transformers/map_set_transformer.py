240c5b197cb0c828af420f3b7f81bf37d393b991
CatGuy1300 <guyv22@gmail.com>
2023-03-01 11:22:59 +0200

    well apperantly ext_set_col class is not needed

---
+++ transformers/map_set_transformer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from typing import Dict, Callable, Tuple
+
+
+class MapSetTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, columns: Dict[str, Tuple[Callable[[any], any], str]]) -> None:
+        self.columns = columns
+    
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        for name in self.columns:
+            X = X.assign(**{name: self.columns[name]})
+        return X
