925d895abd3d4d180ab821187aabf073c4007471
CatGuy1300 <guyv22@gmail.com>
2023-03-08 14:28:26 +0200

    added select pos rows transformer

---
+++ transformers/select_pos_rows_transformer.py
@@ -0,0 +1,26 @@
+from typing import List
+from typing import Union
+
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+
+class SelectPosRowsTransformer(BaseEstimator, TransformerMixin):
+    """
+    Selects given columns/rows corresponding to boolian data seires (should be with same number of rows as the transform target)
+    """
+    def __init__(self, select_by :str, target: str = None) -> None:
+        self.select_by = select_by
+        self.target = target
+    
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        result = None
+        if self.target != None:
+            result = X.copy()
+            result[self.target] = X[self.target][X[self.target][self.select_by]]
+        else:
+            result = X[X[self.select_by]]
+        return result
