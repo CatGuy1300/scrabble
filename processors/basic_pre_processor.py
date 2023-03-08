f284aaf0df44768803171c356a5ae460c72f6b88
CatGuy1300 <guyv22@gmail.com>
2023-03-08 14:29:51 +0200

    added preprocessor  to make it easier to work with data

---
+++ processors/basic_pre_processor.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from typing import Union, Tuple
+
+
+class BasicPreProcessor:
+    """
+    Preprocesses the data using given pipleline, games and turns dfs.
+    Assumes that pipline returns dict contained processed games, turns, train dfs.
+    
+    returns proccesed games, turns and ratings.
+    """
+    def __init__(self, pipeline: Pipeline, games: pd.DataFrame, turns: pd.DataFrame, g_name: str = 'games', t_name: str = 'turns') -> None:
+        self.pipeline = pipeline
+        self.games = games
+        self.turns = turns
+        self.g_name = g_name
+        self.t_name = t_name
+
+    
+    def process(self, data: Union[pd.DataFrame, pd.Series], data_name: str) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series]:
+        new_data_dict = self.pipeline.transform({data_name: data, self.g_name: self.games, self.t_name: self.turns})
+        return new_data_dict[self.g_name], new_data_dict[self.t_name], new_data_dict[data_name]['rating']
--- transformers/map_set_transformer.py
+++ transformers/map_set_transformer.py
@@ -5,13 +5,21 @@ from transformers.columns_setter_transformer import ColumnsSetterTransformer
 
 
 class MapSetTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, columns: Dict[str, Tuple[Callable[[any], any], str]]) -> None:
+    def __init__(self, columns: Dict[str, Tuple[Callable[[any], any], str]], target: str = None) -> None:
         self.columns = columns
+        self.target = target
     
 
     def fit(self, X, y=None):
         return self
     
     def transform(self, X, y=None):
-        return ColumnsSetterTransformer({new_col: lambda df, mapper=mapper, old_col=old_col: df[old_col].apply(mapper)
+        result = None
+        if self.target != None:
+            result = X.copy()
+            result[self.target] = ColumnsSetterTransformer({new_col: lambda df, mapper=mapper, old_col=old_col: df[old_col].apply(mapper)
+                                         for new_col, (mapper, old_col) in self.columns.items()}).transform(X[self.target])
+        else:
+            result = ColumnsSetterTransformer({new_col: lambda df, mapper=mapper, old_col=old_col: df[old_col].apply(mapper)
                                          for new_col, (mapper, old_col) in self.columns.items()}).transform(X)
+        return result
